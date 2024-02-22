a2f97f33700ad9eae3d6b7394dd71f5d6c7c8c5f
elenalambert <elena.maria.lambert@gmail.com>
2024-02-21 20:25:57 -0500

    first commit

---
+++ src/app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request
+#from flask import send_from_directory
+
+
+app = Flask(__name__,static_url_path='/static')
+
+@app.route("/")
+def main():
+    return '''
+     <form action="/echo_input" method="POST">
+         <input name="user_input">
+         <input type="submit" value="Submit!">
+     </form>
+     <img width="200" src="/static/img/elena.png"></img>
+
+     '''
+
+@app.route("/echo_input", methods=["POST"])
+def echo_input():
+    input_text = request.form.get("user_input", "")
+    if not len(input_text):
+        input_text = 'Elena says Hi'
+    return "You entered: " + input_text
+
+
+
